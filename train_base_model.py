8ad6430efdf6d4eba96230b649da782d12aeb34e
Erik <eekihenriksson@gmail.com>
2024-09-27 10:57:19 +0300

    Update train_base_model.py

--- train_base_model.py
+++ train_base_model.py
@@ -109,7 +109,7 @@ def run(args):
     suffix = "_multiclass" if args.multiclass else "_binary"
     class_weights = "_class_weights" if args.use_class_weights else ""
     saved_model_name = (
-        f"base_model{suffix}_clean_ratio_{args.downsample_ratio}{class_weights}"
+        f"base_model{suffix}_clean_ratio_{args.downsample_clean_ratio}{class_weights}"
     )
 
     num_labels = len(label_encoder.classes_)

b1d4cdc4a452cbedd7b73e9e89eaf2f7ab6b9e3d
Erik <eekihenriksson@gmail.com>
2024-10-14 13:45:08 +0300

    Update train_base_model.py

--- train_base_model.py
+++ train_base_model.py
@@ -49,7 +49,7 @@ class CustomSequenceClassification(PreTrainedModel):
         config = CustomConfig(
             num_labels=num_labels,
             use_mean_pooling=use_mean_pooling,
-            **base_config.to_dict(),  # Copies all attributes from the base config
+            #**base_config.to_dict(),  # Copies all attributes from the base config
         )
 
         # Initialize the superclass with the custom config
